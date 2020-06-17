b5a77e335c0db609a41a66c188a5b579c72ff2c1
김대환 <hammer.kim@nuritelecom.com>
2020-06-17 21:14:13 +0900

    mongos

--- mongodb-4.0.x/mongos.py
+++ mongodb-4.0.x/mongos.py
@@ -8,20 +8,10 @@ import time
 import subprocess
 import re
 import urllib
-import platform
-import os
+import io
 
-ZBSERVER = '127.0.0.1'
+ZBSERVER = 'nngp005'
 ZBPORT = 10051
-
-os_ = platform.system()
-
-#for item in os.environ:
-#    print("%s = %s" % (item, os.environ[item]))
-
-#print(" TEMP = " + os.environ["TEMP"])
-
-
 
 #sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 #sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
@@ -58,23 +48,18 @@ state = 0
 
 # Read saved opcounters from previous check
 try:
-    temp_dir = os.environ["TEMP"]
-    if os_ == "Windows":
-        #f = open("C:\\Users\\hammer\\" + mongohost + "-mongos-opcounters")
-        f = open(temp_dir + "\\" + mongohost + "-mongos-opcounters")
-    elif os_ == "Linux":
-        f = open("/tmp/" + mongohost + "-mongos-opcounters")
-
+    #f = open("/tmp/" + mongohost + "-mongos-opcounters")
+    f = open("C:\\Users\\hammer\\" + mongohost + "-mongos-opcounters")
     s = f.read()
     f.close()
-    ts, _insert, _query, _update, _delete, _getmore, _command, _commandRepl, _flushes, _vsize, _rsize, _q_readers, _q_writers, _a_readers, _a_writers, \
-    _net_in, _net_out, _conn = s.split(" ")
+    ts, insert, query, update, delete, getmore, command, commandRepl, flushes, vsize, res, q_readers, q_writers, a_readers, a_writers, \
+    net_in, net_out, conn = s.split(" ")
 except Exception as e:
-    ts, _insert, _query, _update, _delete, _getmore, _command, _commandRepl, _flushes, _vsize, _rsize, _q_readers, _q_writers, _a_readers, _a_writers,  _net_in, _net_out, _conn = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ts, insert, query, update, delete, getmore, command, commandRepl, flushes, vsize, res, q_readers, q_writers, a_readers, a_writers,  net_in, net_out, conn = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     print(e)
 
 print("History opcounters")
-print(ts, _insert, _query, _update, _delete, _getmore, _command, _commandRepl, _flushes, _vsize, _rsize, _q_readers, _q_writers, _a_readers, _a_writers, _net_in, _net_out, _conn)
+print(ts, insert, query, update, delete, getmore, command, commandRepl, flushes, vsize, res, q_readers, q_writers, a_readers, a_writers, net_in, net_out, conn)
 
 # Get serverStatus stats
 try:
@@ -87,84 +72,74 @@ except Exception as e:
 res = mo.admin.command('serverStatus')
 now = time.time()
 
-insert  = int((float(res['opcounters']['insert'])))
-query   = int((float(res['opcounters']['query'])))
-update  = int((float(res['opcounters']['update'])))
-delete  = int((float(res['opcounters']['delete'])))
-getmore = int((float(res['opcounters']['getmore'])))
-command = int((float(res['opcounters']['command'])))
+_insert  = int((float(res['opcounters']['insert'])))
+_query   = int((float(res['opcounters']['query'])))
+_update  = int((float(res['opcounters']['update'])))
+_delete  = int((float(res['opcounters']['delete'])))
+_getmore = int((float(res['opcounters']['getmore'])))
+_command = int((float(res['opcounters']['command'])))
 
 # not in monogos
-commandRepl = 0
+_commandRepl = 0
 # not in monogos
-flushes = 0
+_flushes = 0
 
-vsize= int((float(res['mem']['virtual'])))
-rsize= int((float(res['mem']['resident'])))
+_vsize= int((float(res['mem']['virtual'])))
+_res= int((float(res['mem']['resident'])))
 
 # not in monogos
-q_readers = 0
-q_writers = 0
-a_readers = 0
-a_writers = 0
+_q_readers = 0
+_q_writers = 0
+_a_readers = 0
+_a_writers = 0
 
 #_q_readers= int((float(res['globalLock']['currentQueue']['readers'])))
-#_q_writers= int((float(res['globalLock']['currentQueue']['Zwriters'])))
+#_q_writers= int((float(res['globalLock']['currentQueue']['Xwriters'])))
 #_a_readers= int((float(res['globalLock']['activeClients']['readers'])))
 #_a_writers= int((float(res['globalLock']['activeClients']['writers'])))
 
-net_in= int((float(res['network']['bytesIn'])))
-net_out= int((float(res['network']['bytesOut'])))
+_net_in= int((float(res['network']['bytesIn'])))
+_net_out= int((float(res['network']['bytesOut'])))
 
-conn=int((float(res['connections']['current'])))
+_conn=int((float(res['connections']['current'])))
 
-insert_  = int((float(insert) - float(_insert))/((now - float(ts))))
-query_   = int((float(query) - float(_query))/((now - float(ts))))
-update_  = int((float(update) - float(_update))/((now - float(ts))))
-delete_  = int((float(delete) - float(_delete))/((now - float(ts))))
-getmore_ = int((float(getmore) - float(_getmore))/((now - float(ts))))
-command_ = int((float(command) - float(_command))/((now - float(ts))))
-commandRepl_ =  int((float(commandRepl) - float(_commandRepl))/((now - float(ts))))
+insert_  = int((float(_insert) - float(insert))/((now - float(ts))))
+query_   = int((float(_query) - float(query))/((now - float(ts))))
+update_  = int((float(_update) - float(update))/((now - float(ts))))
+delete_  = int((float(_delete) - float(delete))/((now - float(ts))))
+getmore_ = int((float(_getmore) - float(getmore))/((now - float(ts))))
+command_ = int((float(_command) - float(command))/((now - float(ts))))
+commandRepl_ =  int((float(_commandRepl) - float(commandRepl))/((now - float(ts))))
 
-flushes_ =  int(flushes)
+flushes_ =  int(_flushes)
+vsize_   =  int(_vsize)
+res_   =  int(_res)
 
-# unit is Megabytes, so convert to bytes
-vsize_   =  int(vsize) * 1024 * 1024
-rsize_   =  int(rsize) * 1024 * 1024
-
-q_readers_ = int(q_readers)
-q_writers_ = int(q_writers)
-a_readers_ = int(a_readers)
-a_writers_ = int(a_writers)
-net_in_    = int((float(net_in) - float(_net_in))/((now - float(ts))))
-net_out_   = int((float(net_out) - float(_net_out))/((now - float(ts))))
-conn_      = int(conn)
+q_readers_ = int(_q_readers)
+q_writers_ = int(_q_writers)
+a_readers_ = int(_a_readers)
+a_writers_ = int(_a_writers)
+net_in_    = int((float(_net_in) - float(net_in))/((now - float(ts))))
+net_out_   = int((float(_net_out) - float(net_out))/((now - float(ts))))
+conn_      = int(_conn)
 
 # Save opcounters
 try:
-    if os_ == "Windows":
-        temp_dir = os.environ["TEMP"]
-        #f = open("C:\\Users\\hammer\\" + mongohost + "-mongos-opcounters", 'w')
-        f = open(temp_dir + "\\" + mongohost + "-mongos-opcounters", 'w')
-    elif os_ == "Linux":
-        f = open("/tmp/" + mongohost + "-mongos-opcounters", 'w')
-
-    f.write(str(int(now)) + ' ' + str(insert) + ' ' + str(query) + ' ' + str(update) + ' ' + \
-            str(delete) + ' ' + str(getmore) + ' ' + str(command) + ' ' + str(commandRepl) + ' ' +\
-            str(flushes) + ' ' + str(vsize) + ' ' + str(rsize) + ' ' +\
-            str(q_readers) + ' ' + str(q_writers) + ' ' + str(a_readers) + ' ' + str(a_writers)  + ' ' +\
-            str(net_in) + ' ' + str(net_out) + ' ' + str(conn)
+    #f = open("/tmp/" + mongohost + "-mongos-opcounters", 'w')
+    f = open("C:\\Users\\hammer\\" + mongohost + "-mongos-opcounters", 'w')
+    f.write(str(int(now)) + ' ' + str(_insert) + ' ' + str(_query) + ' ' + str(_update) + ' ' + \
+            str(_delete) + ' ' + str(_getmore) + ' ' + str(_command) + ' ' + str(_commandRepl) + ' ' +\
+            str(_flushes) + ' ' + str(_vsize) + ' ' + str(_res) + ' ' +\
+            str(_q_readers) + ' ' + str(_q_writers) + ' ' + str(_a_readers) + ' ' + str(_a_writers)  + ' ' +\
+            str(_net_in) + ' ' + str(_net_out) + ' ' + str(_conn)
             )
     f.close()
 except Exception as e:
     print("Can't update stats!")
     print(e)
-    sys.exit(1)
 
-mongos_total_ops = int(insert_) + int(query_) + int(update_) + int(delete_) + int(getmore_) + int(command_)
-print( insert_, query_, update_, delete_, getmore_, command_, commandRepl_, flushes_, vsize_, rsize_, q_readers_, q_writers_, a_readers_, a_writers_, net_in_, net_out_, conn_)
+print( insert_, query_, update_, delete_, getmore_, command_, commandRepl_, flushes_, vsize_, res_, q_readers_, q_writers_, a_readers_, a_writers_, net_in_, net_out_, conn_)
 
-#mongos_total_ops=0
 err = 'OK'
 state = 1
 
@@ -178,17 +153,17 @@ packet.append(ZabbixMetric(zbhost, "mongos_delete", int(delete_)))
 packet.append(ZabbixMetric(zbhost, "mongos_getmore", int(getmore_)))
 packet.append(ZabbixMetric(zbhost, "mongos_command", int(command_)))
 packet.append(ZabbixMetric(zbhost, "mongos_commandRepl", int(commandRepl_)))
-packet.append(ZabbixMetric(zbhost, "mongos_flushes", int(flushes_)))
-packet.append(ZabbixMetric(zbhost, "mongos_vsize", int(vsize_)))
-packet.append(ZabbixMetric(zbhost, "mongos_rsize", int(rsize_)))
-packet.append(ZabbixMetric(zbhost, "mongos_qReaders", int(q_readers_)))
-packet.append(ZabbixMetric(zbhost, "mongos_qWriters", int(q_writers_)))
-packet.append(ZabbixMetric(zbhost, "mongos_aReaders", int(a_readers_)))
-packet.append(ZabbixMetric(zbhost, "mongos_aWriters", int(a_writers_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(flushes_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(vsize_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(res_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(q_readers_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(q_writers_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(a_readers_)))
+packet.append(ZabbixMetric(zbhost, "mongos_insert", int(a_writers_)))
 packet.append(ZabbixMetric(zbhost, "mongos_netin", int(net_in_)))
 packet.append(ZabbixMetric(zbhost, "mongos_netout", int(net_out_)))
 packet.append(ZabbixMetric(zbhost, "mongos_conn", int(conn_)))
-packet.append(ZabbixMetric(zbhost, "mongos_total_ops", int(mongos_total_ops)))
+packet.append(ZabbixMetric(zbhost, "mongos_total_ops", int(0)))
 
 try:
     t = ZabbixSender(zabbix_port = ZBPORT, zabbix_server = ZBSERVER).send(packet)
